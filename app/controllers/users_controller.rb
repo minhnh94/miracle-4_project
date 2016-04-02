5e3f0d3ec54a5bca23bc697e48d221bba6139763
Minh Hoang <adsfdsaaa@gmail.com>
2016-04-02 12:17:56 +0000

    add image upload function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -87,6 +87,7 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
+    
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless @user == current_user
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328030238) do
+ActiveRecord::Schema.define(version: 20160402101554) do
+
+  create_table "images", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
