f29b9d490a0517469ba487e6ee8115eccabee3c1
ducngocvnk57 <ducngocvnk57@gmail.com>
2016-04-24 16:18:54 +0000

    fix

--- app/models/image.rb
+++ app/models/image.rb
@@ -7,6 +7,6 @@ class Image < ActiveRecord::Base
     belongs_to :user
     has_many :image_comment, ->{ order "created_at desc"}
     def self.search(search)
-        where("title LIKE ?", "%#{search}%")
+        where("title  LIKE ?", "%#{search}%")
     end    
 end
